2002df8e04d8e560bcdb8dee3cbc7c8be5b422f3
Nathan Blair <nateblairfilm@gmail.com>
2022-01-14 16:23:30 -0600

    Copy source files to demo folder for compatibility with Safari local dev setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require( 'path' );
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -6,4 +7,18 @@ module.exports = {
         path: path.resolve( __dirname, 'dist' ),
         filename: 'pushin.js',
     },
+    plugins: [
+        new FileManagerPlugin({
+            events: {
+                onEnd: [{
+                    copy: [
+                        {
+                            source: path.join(__dirname, 'dist'),
+                            destination: path.join(__dirname, 'demo')
+                        }
+                    ]
+                }]
+            }
+        })
+    ]
 };
