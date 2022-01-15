56486f2ebf6bd8a194978d06f2a423733ed9348e
Nathan Blair <nateblairfilm@gmail.com>
2022-01-15 12:11:14 -0600

    Add package info to build file + rename to .min.js extension

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
 const path = require( 'path' );
 const FileManagerPlugin = require('filemanager-webpack-plugin');
+const webpack = require( 'webpack' );
+const TerserPlugin = require("terser-webpack-plugin");
+
+const PACKAGE = require('./package.json');
 
 module.exports = {
     mode: 'production',
     output: {
         path: path.resolve( __dirname, 'dist' ),
-        filename: 'pushin.js',
+        filename: 'pushin.min.js',
     },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            })
+        ],
+      },
     plugins: [
+        new webpack.BannerPlugin( {
+            banner: `Pushin.js - v${PACKAGE.version}\nAuthor: ${PACKAGE.author}\nLicense: ${PACKAGE.license}`,
+        } ),
         new FileManagerPlugin({
             events: {
                 onEnd: [{
