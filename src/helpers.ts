b7482282cb3dd46a0edb391b2e1e07a5f08fbe58
Nathan Blair <nateblairfilm@gmail.com>
2022-04-13 17:58:15 -0500

    #21: Deprecate support for custom selectors

--- src/helpers.ts
+++ src/helpers.ts
@@ -10,22 +10,11 @@ declare global {
 /**
  * Helper function: Set up and start push-in effect on all elements
  * matching the provided selector.
- *
- * @param options - Optional (legacy) - specify a unique selector for the container of the effect
  */
-window.pushInStart = (options?: PushInOptions | string): PushIn[] => {
-  let selector = '.pushin';
-  let pushInOptions: PushInOptions | undefined;
-  if (options) {
-    // Backward compatibility <3.3.0 - first parameter was selector, not options
-    if (typeof options === 'string') {
-      selector = options;
-    } else {
-      pushInOptions = options;
-    }
-  }
+window.pushInStart = (options?: PushInOptions): PushIn[] => {
+  const pushInOptions = options ?? {};
 
-  const elements = document.querySelectorAll<HTMLElement>(selector);
+  const elements = document.querySelectorAll<HTMLElement>('.pushin');
   const instances: PushIn[] = [];
   for (const element of elements) {
     const instance = new PushIn(element, pushInOptions);
