4dffedca934ef6ec06d6b312970c300619e1bdc0
Nathan Blair <nateblairfilm@gmail.com>
2022-05-04 22:19:06 -0500

    #69: Update unit tests for getOutpoints and getScaleValue methods

--- test/getOutpoints.spec.ts
+++ test/getOutpoints.spec.ts
@@ -1,69 +1,69 @@
 import { setupJSDOM } from './setup';
-import { PushIn } from '../src/pushin';
+import { PushInScene } from '../src/pushInScene';
+import { PushInLayer } from '../src/pushInLayer';
 
 describe('getInpoints', () => {
-  let pushIn: PushIn;
+  let mockPushInLayer: PushInLayer;
 
   beforeEach(() => {
     setupJSDOM(`
-        <!DOCTYPE html>
-            <body>
-                <div class="pushin">
-                    <div class="pushin-scene">
-                        <div id="layer-0" class="pushin-layer">Layer 0</div>
-                        <div id="layer-1" class="pushin-layer" data-pushin-to="300">Layer 1</div>
-                        <div id="layer-2" class="pushin-layer" data-pushin-to="300,500">Layer 2</div>
-                        <div id="layer-3" class="pushin-layer">Layer 3</div>
-                    </div>
-                </div>
-            </body>
-        </html>`);
+      <!DOCTYPE html>
+        <body>
+          <div class="pushin">
+            <div class="pushin-scene">
+              <div id="layer-0" class="pushin-layer">Layer 0</div>
+              <div id="layer-1" class="pushin-layer" data-pushin-to="300">Layer 1</div>
+              <div id="layer-2" class="pushin-layer" data-pushin-to="300,500">Layer 2</div>
+              <div id="layer-3" class="pushin-layer">Layer 3</div>
+            </div>
+          </div>
+        </body>
+      </html>`);
 
-    pushIn = new PushIn(null);
+    mockPushInLayer = Object.create(PushInLayer.prototype);
+    mockPushInLayer['scene'] = <PushInScene>{};
   });
 
-  afterEach(() => pushIn.destroy());
-
   it('Should return inpoint + layerDepth by default for first layer', () => {
-    pushIn['layerDepth'] = 300;
+    mockPushInLayer['scene']['layerDepth'] = 300;
 
     const inpoint = 100;
 
     const elem = document.querySelector<HTMLElement>('#layer-0');
-    const result = pushIn['getOutpoints'](elem, inpoint, 0);
+    const result = mockPushInLayer['getOutpoints'](elem, inpoint);
 
     expect(result).toEqual([400]);
   });
 
   it('Should return data-attribute value if set', () => {
-    pushIn['layerDepth'] = 300;
+    mockPushInLayer['scene']['layerDepth'] = 300;
 
     const inpoint = 100;
 
     const elem = document.querySelector<HTMLElement>('#layer-1');
-    const result = pushIn['getOutpoints'](elem, inpoint, 1);
+    const result = mockPushInLayer['getOutpoints'](elem, inpoint);
 
     expect(result).toEqual([300]);
   });
 
   it('Should return array of data from data-attribute if set', () => {
-    pushIn['layerDepth'] = 300;
+    mockPushInLayer['scene']['layerDepth'] = 300;
 
     const inpoint = 100;
 
     const elem = document.querySelector<HTMLElement>('#layer-2');
-    const result = pushIn['getOutpoints'](elem, inpoint, 2);
+    const result = mockPushInLayer['getOutpoints'](elem, inpoint);
 
     expect(result).toEqual([300, 500]);
   });
 
   it('Should generate value based on previous inpoint', () => {
-    pushIn['layerDepth'] = 300;
+    mockPushInLayer['scene']['layerDepth'] = 300;
 
     const inpoint = 500;
 
     const elem = document.querySelector<HTMLElement>('#layer-3');
-    const result = pushIn['getOutpoints'](elem, inpoint, 3);
+    const result = mockPushInLayer['getOutpoints'](elem, inpoint);
 
     expect(result).toEqual([800]);
   });
--- test/getScaleValue.spec.ts
+++ test/getScaleValue.spec.ts
@@ -1,56 +1,59 @@
 import { setupJSDOM } from './setup';
+import { PushInLayer } from '../src/pushInLayer';
+import { PushInScene } from '../src/pushInScene';
 import { PushIn } from '../src/pushin';
-import { PushInLayer } from '../src/types';
+import { LayerParams } from '../src/types';
 
 describe('getScaleValue', () => {
-  let pushIn: PushIn;
-  const layerMock = {
-    originalScale: 2,
-    params: {
-      inpoint: 10,
-      speed: 2,
-    },
-  } as PushInLayer;
-
-  const layerMock2 = {
-    originalScale: 1,
-    params: {
-      inpoint: 150,
-      speed: 100,
-    },
-  } as PushInLayer;
+  let layerMock: PushInLayer;
+  let layerMock2: PushInLayer;
+  let mockPushInLayer: PushInLayer;
 
   beforeEach(() => {
     setupJSDOM(`<!DOCTYPE html></html>`);
-    pushIn = new PushIn(null);
+    layerMock = Object.create(PushInLayer.prototype);
+    layerMock['originalScale'] = 2;
+    layerMock['params'] = <LayerParams>{
+      inpoint: 10,
+      speed: 2,
+    };
+
+    layerMock2 = Object.create(PushInLayer.prototype);
+    layerMock2['originalScale'] = 1;
+    layerMock2['params'] = <LayerParams>{
+      inpoint: 150,
+      speed: 100,
+    };
+
+    mockPushInLayer = Object.create(PushInLayer.prototype);
+    mockPushInLayer['scene'] = Object.create(PushInScene.prototype);
+    mockPushInLayer['scene']['pushin'] = <PushIn>{scrollY:0};
   });
 
-  afterEach(() => pushIn.destroy());
-
   it('should return original scale if scroll position and inpoint are the same', () => {
-    pushIn['scrollY'] = 10;
-    const result = pushIn['getScaleValue'](layerMock);
+    mockPushInLayer['scene']['pushin']['scrollY'] = 10;
+    const result = mockPushInLayer['getScaleValue'](layerMock);
 
     expect(result).toEqual(2);
   });
 
   it('should reduce scale if scrollY is less than inpoint', () => {
-    pushIn['scrollY'] = 6;
-    const result = pushIn['getScaleValue'](layerMock);
+    mockPushInLayer['scene']['pushin']['scrollY'] = 6;
+    const result = mockPushInLayer['getScaleValue'](layerMock);
 
     expect(result).toEqual(1.9992);
   });
 
   it('should increase scale if scrollY is greater than inpoint', () => {
-    pushIn['scrollY'] = 20;
-    const result = pushIn['getScaleValue'](layerMock);
+    mockPushInLayer['scene']['pushin']['scrollY'] = 20;
+    const result = mockPushInLayer['getScaleValue'](layerMock);
 
     expect(result).toEqual(2.002);
   });
 
   it('should not return a negative number', () => {
-    pushIn['scrollY'] = 1;
-    const result = pushIn['getScaleValue'](layerMock2);
+    mockPushInLayer['scene']['pushin']['scrollY'] = 1;
+    const result = mockPushInLayer['getScaleValue'](layerMock2);
 
     expect(result).toEqual(0);
   });
