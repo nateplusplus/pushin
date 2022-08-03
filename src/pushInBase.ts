0a354a553586d47c314a1b850c83699ebdb17c95
Nathan Blair <nateblairfilm@gmail.com>
2022-08-02 22:27:31 -0500

    #223: Use abstract class for composition

--- src/pushInBase.ts
+++ src/pushInBase.ts
@@ -4,11 +4,18 @@ export default abstract class PushInBase {
   /**
    * Get the value for an option from either HTML markup or the JavaScript API
    */
-  getOption(name: string, options: any): string | number | boolean | undefined {
+  getOption(
+    name: string,
+    options: any
+  ): string | string[] | number | number[] | boolean | undefined {
     let value;
     const attribute = this.getAttributeName(name);
     if (this.container.hasAttribute(attribute)) {
       value = <string>this.container!.dataset!.pushinScrollTarget;
+      const array = value.split(',');
+      if (array.length > 1) {
+        value = array;
+      }
     } else if (options[name]) {
       value = options[name];
     }
