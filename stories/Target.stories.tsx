87232ad9a44568b6f6833e628b88767bfa0ea601
Nathan Blair <nateblairfilm@gmail.com>
2023-03-04 16:50:27 -0600

    #333: Add storybook and simple tests

---
+++ stories/Target.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Target } from './Target';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Target',
+  component: Target,
+  argTypes: {
+    scrollTarget: { control: 'text' },
+  },
+} as ComponentMeta<typeof Target>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Target> = args => <Target {...args} />;
+
+export const Default = Template.bind({});
+
+export const WindowScroll = Template.bind({});
+WindowScroll.args = {
+  scrollTarget: 'window',
+};
